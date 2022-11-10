b5d7426e1c838b6401c939f76a5288938ea47638
Adauto Donizete Junior - Business Intelligence Lev Negócios <analista.bi@levnegocios.com.br>
2022-11-09 22:00:19 -0300

    fix name dir estabelicimentos

--- main.py
+++ main.py
@@ -12,13 +12,10 @@ spark = SparkSession \
 
 basePath = os.getcwd()
 pathCompany = os.path.join(basePath, 'data', 'empresas', '*.csv')
-pathEstablishment = os.path.join(basePath, 'data', 'estabelicimento', '*.csv')
+pathEstablishment = os.path.join(basePath, 'data', 'estabelecimentos', '*.csv')
 pathPartners = os.path.join(basePath, 'data', 'socios', '*.csv')
 
 dfCompany = spark.read.csv(pathCompany, sep=';', inferSchema=True)
 dfEstablishment = spark.read.csv(pathEstablishment, sep=';', inferSchema=True)
 dfPartners = os.path.join(pathPartners, 'data', '*.csv')
 
-print(dfCompany.count())
-print(dfEstablishment.count())
-print(dfPartners.count())
