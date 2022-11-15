099f9aad07f703140020036b2e33e808dcb7ae8a
Adauto Donizete Junior - Business Intelligence Lev Negócios <analista.bi@levnegocios.com.br>
2022-11-15 11:37:17 -0300

    finishing class 4

--- main.py
+++ main.py
@@ -38,9 +38,6 @@ for index, colName in enumerate(colNamesPartners):
 dfCompany = dfCompany.withColumn('CAPITAL_SOCIAL', f.regexp_replace('CAPITAL_SOCIAL', ',', '.'))
 dfCompany = dfCompany.withColumn('CAPITAL_SOCIAL', dfCompany['CAPITAL_SOCIAL'].cast(DoubleType()))
 
-dfCompany.printSchema()
-print(dfCompany.limit(5).toPandas())
-
 dfEstablishment = dfEstablishment \
     .withColumn(
         'DATA_SITUACAO_CADASTRAL', 
@@ -53,13 +50,72 @@ dfEstablishment = dfEstablishment \
         f.to_date(dfEstablishment['DATA_DA_SITUACAO_ESPECIAL'].cast(StringType()), 'yyyyMMdd')
     )
 
-dfEstablishment.printSchema()
-print(dfEstablishment.limit(5).toPandas())
-
 dfPartners = dfPartners.withColumn(
     'DATA_DE_ENTRADA_SOCIEDADE',
     f.to_date(dfPartners['DATA_DE_ENTRADA_SOCIEDADE'].cast(StringType()), 'yyyyMMdd')
 )
 
-dfPartners.printSchema()
-print(dfPartners.limit(5).toPandas())
+# dfCompany.printSchema()
+# print(dfCompany.limit(5).toPandas())
+
+# dfEstablishment.printSchema()
+# print(dfEstablishment.limit(5).toPandas())
+
+# dfPartners.printSchema()
+# print(dfPartners.limit(5).toPandas())
+
+# print(dfCompany.select('NATUREZA_JURIDICA', 'PORTE').toPandas())
+
+# print(dfPartners.select(
+#     'NOME_DO_SOCIO_OU_RAZAO_SOCIAL', 
+#     'FAIXA_ETARIA',
+#     f.year('DATA_DE_ENTRADA_SOCIEDADE').alias('ANO_DE_ENTRADA')
+# ).toPandas())
+
+# print(dfEstablishment.select(
+#     'NOME_FANTASIA',
+#     'MUNICIPIO',
+#     f.year('DATA_DE_INICIO_ATIVIDADE').alias('ANO_INICIO_ATIVIDADE'),
+#     f.month('DATA_DE_INICIO_ATIVIDADE').alias('MES_INICIO_ATIVIDADE')
+# ).toPandas())
+
+# dfPartners.printSchema()
+
+# dfPartners.select(
+#     [f.count(f.when(f.isnull(c), 1)).alias(c) for c in dfPartners.columns]
+# ).show()
+
+# dfPartners = dfPartners.na.fill(0).limit(5).show()
+# dfPartners = dfPartners.na.fill('-').limit(5).show()
+
+# dfPartners \
+#     .select(
+#         'NOME_DO_SOCIO_OU_RAZAO_SOCIAL', 
+#         'FAIXA_ETARIA',
+#         f.year('DATA_DE_ENTRADA_SOCIEDADE').alias('ANO_DE_ENTRADA')
+#     ).orderBy(['ANO_DE_ENTRADA', 'FAIXA_ETARIA'], ascending=[False, True]) \
+#     .show(10, False)
+
+# dfCompany \
+#     .where('CAPITAL_SOCIAL == 50') \
+#     .show(10, False)
+
+# dfPartners\
+#     .select('NOME_DO_SOCIO_OU_RAZAO_SOCIAL')\
+#     .filter(dfPartners['NOME_DO_SOCIO_OU_RAZAO_SOCIAL'].startswith('RODRIGO'))\
+#     .filter(dfPartners['NOME_DO_SOCIO_OU_RAZAO_SOCIAL'].endswith('DIAS'))\
+#     .show(10, False)
+
+# df = spark.createDataFrame([('RESTAURANTE DO RUI',), ('Juca restaurantes ltda',), ('Joca Restaurante',)], ['data'])
+
+# df.where(f.upper(df['data']).like('RESTAURANTE%')).show(truncate=False)
+
+dfCompany\
+    .select( 
+        'RAZAO_SOCIAL', 
+        'NATUREZA_JURIDICA', 
+        'PORTE',
+        'CAPITAL_SOCIAL',
+    )\
+    .filter(f.upper(dfCompany['RAZAO_SOCIAL']).like('%RESTAURANTE%'))\
+    .show(10, False)
